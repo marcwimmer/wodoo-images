e5c5e9aa004b23d61f6634b2960e6d9124c2157c
Marc Wimmer <marc@itewimmer.de>
2023-04-07 16:39:20 +0200

    dockerformac: dockerformace

--- postgres/run.sh
+++ postgres/run.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -e
 
+function make_entrypoint_with_params() {
 python3 <<EOF
 print("Version 1.0")
 import os
 with open('/config') as file:
-    conf = file.read().split("\n")
+    conf = file.read().splitlines()
 conf += os.getenv('POSTGRES_CONFIG').split(",")
-conf = list(filter(lambda x: bool((x or '').strip()), conf))
+conf = list(filter(lambda x: bool((x or '').strip()) and not (x or '').strip().startswith("#"), conf))
 
 print("Applying configuration:\n" + '\n'.join(conf))
 
@@ -17,6 +18,29 @@ with open('/start.sh', 'w') as f:
     f.write('/usr/local/bin/docker-entrypoint.sh postgres ' + ' '.join(conf))
 
 EOF
+}
+
+function create_socket() {
+    # we let postgres create its socket file with strange permissions as it wants
+    # Using socat with connect that socket file
+    PGSOCKET=/var/run/postgresql/.s.PGSQL.5432
+    while [[ ! -e "$PGSOCKET" ]]; do
+        sleep 0.5
+        echo "Waiting for socketfile"
+    done
+    echo "unix socket $PGSOCKET available now"
+    set -x
+    mkdir -p "$(dirname "$POSTGRES_SOCKET")"
+    if [[ -e "$POSTGRES_SOCKET" ]]; then
+        unlink "$POSTGRES_SOCKET"
+        rm "$POSTGRES_SOCKET" || true
+    fi
+    sync
+    socat unix-listen:$POSTGRES_SOCKET,reuseaddr,fork UNIX-CONNECT:/var/run/postgresql/.s.PGSQL.5432
+}
+
+make_entrypoint_with_params
+create_socket &
 
 if [[ "$1" == "postgres" ]]; then
     exec gosu postgres bash /start.sh
