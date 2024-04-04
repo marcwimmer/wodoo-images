6531b6dd7a20b53bf13f2b6b7bb82146d353f113
Marc Wimmer <marc@zebroo.de>
2024-04-04 08:21:22 +0200

    master: Fixed

--- odoo/bin/run_soffice.sh
+++ odoo/bin/run_soffice.sh
@@ -13,7 +13,7 @@ do
         sudo -u odoo /usr/lib/libreoffice/program/soffice.bin --headless --calc --accept="socket,host=127.0.0.1,port=2002;urp;StarOffice.ServiceManager" &
         PID=$(echo $!)
         disown $PID
-        sleep 1
         kill -0 $PID 2>/dev/null && break
     fi
+    sleep 1
 done
