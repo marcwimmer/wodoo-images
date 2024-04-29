c1b1c72da5d3ad01d2f303d2cbcb82c6805480bd
Marc Wimmer <marc@zebroo.de>
2024-04-29 17:50:00 +0200

    auto install module

--- fluentd/__after_settings.py
+++ fluentd/__after_settings.py
@@ -1,15 +1,16 @@
 import subprocess
+import json
 from pathlib import Path
 import os
 
 def after_settings(settings, config):
+    module_name = 'wodoo_logging'
     gimera = Path(os.getcwd()) / 'gimera.yml'
     url = "git@github.com:Odoo-Ninjas/wodoo-logging"
     if url in gimera.read_text():
         return
     if not config.RUN_FLUENTD:
         return
-    import pudb;pudb.set_trace()
     manifestfile = Path(os.getcwd()) / "MANIFEST"
     manifest = eval(manifestfile.read_text())
     version = str(manifest['version'])
@@ -17,6 +18,11 @@ def after_settings(settings, config):
     subprocess.check_call(["gimera", "add", "-u", url, "-b", version, "-p", path, "-t", "integrated"])
     subprocess.check_call(["gimera", "apply", path])
 
-    if path not in manifest['addons-paths']:
-        manifest['addons-paths'].append(path)
-        manifestfile.write_text(manifest)
+    if path not in manifest['addons_paths']:
+        manifest['addons_paths'].append(path)
+        manifestfile.write_text(str(manifest))
+    if module_name not in manifest['install']:
+        manifest['install'].append(module_name)
+        manifestfile.write_text(str(manifest))
+
+    subprocess.check_call(["odoo", "rewrite"])
