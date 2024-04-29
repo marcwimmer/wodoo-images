80838c1cdc17ce6d2ca59bd02ac48f41c0483c12
Marc Wimmer <marc@zebroo.de>
2024-04-29 10:59:21 +0200

    fluentlogger: Fixed

---
+++ fluentd/__on_event.py
@@ -0,0 +1,5 @@
+import subprocess
+from pathlib import Path
+
+def on_up(config, settings, yml, globals):
+    pass
--- fluentd/init_sql/console_log.sql
+++ fluentd/init_sql/console_log.sql
@@ -1,12 +1,7 @@
 create table console_log (
     id serial primary key,
-    date character varying,
-    ttime character varying,
-    loglevel character varying,
+    col_datetime timestamp,
     line character varying,
-    exported boolean,
     tag character varying
 );
 
-create index console_log_timestamp on console_log(logdate);
-create index console_log_exported on console_log(exported);
