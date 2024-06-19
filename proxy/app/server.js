58aced7849030ba4e8401e949bb5245019cce340
Marc Wimmer <marc@zebroo.de>
2024-06-19 16:55:40 +0200

    master: Fixed

--- proxy/app/server.js
+++ proxy/app/server.js
@@ -25,16 +25,6 @@ function override_favicon(req, res) {
     if (process.env.DEVMODE === "1") {
         if (req.path.indexOf("favicon.ico") > 0 || req.path.indexOf("/favicon/") >= 0) {
             const path_icon = path.join(__dirname, 'favicon_dev.png');
-            const textToImage = require('text-to-image');
-            // using the asynchronous API with await
-            const dataUri = await textToImage.generate('Lorem ipsum dolor sit amet');
-            // using the asynchronous API with .then
-            textToImage.generate('Lorem ipsum dolor sit amet').then(function (dataUri) {
-            // use the dataUri
-            });
-            // using the synchronous API
-            const dataUri = textToImage.generateSync('Lorem ipsum dolor sit amet');
-
             res.sendFile(path_icon);
             return true;
         }
