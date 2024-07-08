545bae4c864e075056fef9618f76ad1c42a7bce7
Marc Wimmer <marc@zebroo.de>
2024-07-08 09:58:19 +0200

    starting up works

--- vscode/entrypoint.sh
+++ vscode/entrypoint.sh
@@ -3,13 +3,21 @@ if [[ "$DEVMODE" == "1" ]]; then
 DISPLAY=:1.0
 export DISPLAY
 
+killall xvfb
+killall x11vnc
+killall vscode
+ps aux
+rm /tmp/.*lock /tmp/*lock /tmp/.*X11* /tmp/*vscode* -R
+rm /tmp/.X11-unix
+ls /tmp -lhtra
+
 # x authority
 COOKIE=$(mcookie)
 TEMP_XAUTH="/tmp/.Xauthority-$USERNAME"
 if [[ -e "$TEMP_XAUTH" ]]; then
 	rm "$TEMP_XAUTH"
 fi
-rm $USER_HOME/.Xauthority || true
+[[ -f $USER_HOME/.Xauthority ]] && rm $USER_HOME/.Xauthority
 xauth -f $TEMP_XAUTH add $DISPLAY . $COOKIE
 USER_HOME=$(eval echo ~$USERNAME)
 mv $TEMP_XAUTH $USER_HOME/.Xauthority
@@ -20,8 +28,8 @@ rsync $USER_HOME/.vnc/ /root/.vnc/ -ar
 
 ls -lhtra /root/.vnc
 
-Xvfb :1 -screen 0 "${DISPLAY_WIDTH}x${DISPLAY_HEIGHT}x16" &
-/usr/bin/x11vnc -display :1.0 -auth guess -forever -rfbport 5900 -rfbauth /root/.vnc/passwd &
+Xvfb $DISPLAY -screen 0 "${DISPLAY_WIDTH}x${DISPLAY_HEIGHT}x16" &
+/usr/bin/x11vnc -display ${DISPLAY} -auth guess -forever -rfbport 5900 -rfbauth /root/.vnc/passwd &
 code-insiders --no-sandbox --user-data-dir /codedata
 sleep infinity
 fi
