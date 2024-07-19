93a0a820e5f19a99a72f9a568ec579e1cf022a29
Marc Wimmer <marc@zebroo.de>
2024-07-19 10:11:27 +0200

    basically works

--- webssh/launch.sh
+++ webssh/launch.sh
@@ -6,7 +6,6 @@ service nginx start
 service ssh start
 
 set -r
-export WEBSSH2_BASEURL=/console
 wssh \
 --address=0.0.0.0 \
 --port=8080 \
@@ -15,5 +14,4 @@ wssh \
 --xheaders=True \
 --debug \
 --wpintvl=3600 \
---redirect false \
---baseURLL /console
+--redirect false
