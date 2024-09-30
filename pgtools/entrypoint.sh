43c0e9cf5d28848422e992b2de0094f114846098
github-action@github.com <github-action@github.com>
2024-09-30 08:56:32 +0200

    removed cat

--- pgtools/entrypoint.sh
+++ pgtools/entrypoint.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-set -x
 
 active_file="/tmp/pgcli_history.active"
-store_file="/tmp/pgcli_history"
+store_file="/tmp/pgcli_history/pgcli_history.active"
 
-if [[ ! -f "$active_file" ]]; then
-	if [[ -f "$store_file" ]]; then
-		cp "$store_file" "$active_file"
-	fi
+if [[ -f "$store_file" ]]; then
+	# echo "Restoring history file $store_file to $active_file"
+	cp "$store_file" "$active_file"
 fi
 
 "$@"
-
+retcode="$?"
 
 if [[ -f "$active_file" ]]; then
+	# echo "Storing history file $active_file to $store_file"
 	cp "$active_file" "$store_file"
+fi
+
+if [[ "$retcode" != "0" ]]; then
+	exit "$retcode"
 fi
