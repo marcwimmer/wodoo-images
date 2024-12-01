c1d84af57517fe11ea57e8488294ab3acfeb0838
github-action@github.com <github-action@github.com>
2024-12-01 20:02:36 +0100

    V18

--- theia_odoo_vim/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# collect provided vsix files
-rsync -v /home/vsix_files --include=*.vsix /home/theia/plugins
-
-node \
-	/home/theia/src-gen/backend/main.js \
-	/home/project \
-	--hostname=0.0.0.0
